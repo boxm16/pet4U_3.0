192623b6dcfb961552e0268e9a6cfacb6415d60e
Michail Sitmalidis <boxm16@yahoo.com>
2023-08-15 16:45:23 +0300

    push commit

--- src/java/StockManagement/StockManagementControlle.java
+++ src/java/StockManagement/StockManagementControlle.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package StockManagement;
 
-/**
- *
- * @author Michail Sitmalidis
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class StockManagementControlle {
-    
+
+    @RequestMapping(value = "suppliersAndStockManagementDashboard")
+    public String suppliersAndStockManagementDashboard() {
+        return "stockManagement/suppliersDashboard";
+    }
 }
