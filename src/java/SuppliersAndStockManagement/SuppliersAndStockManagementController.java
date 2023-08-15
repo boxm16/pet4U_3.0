18b0931e5ed586925bf3df2d0e7bc627c5a2bc01
Michail Sitmalidis <boxm16@yahoo.com>
2023-08-15 17:47:20 +0300

    push commit

--- src/java/SuppliersAndStockManagement/SuppliersAndStockManagementController.java
+++ src/java/SuppliersAndStockManagement/SuppliersAndStockManagementController.java
@@ -1,26 +1,13 @@
 package SuppliersAndStockManagement;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SuppliersAndStockManagementController {
-
-    @RequestMapping(value = "suppliersAndStockManagementDashboard")
-    public String suppliersAndStockManagementDashboard() {
-        return "suppliersAndStockManagement/suppliersAndStockManagementDashboard";
-    }
-
-    @RequestMapping(value = "ggg")
-    public String ff() {
+  @RequestMapping(value = "iva")
+    public String inventoryDashboard() {
 
         return "inventory/inventoryDashboard";
-    }
-
-    @RequestMapping(value = "royalStockManagement")
-    public String royalStockManagement(ModelMap modelMap) {
-        modelMap.addAttribute("supplier", "Royal");
-        return "suppliersAndStockManagement/stockManagement";
     }
 }
