c4596cd7dd361742e2645aea44810692d0551187
Michail Sitmalidis <boxm16@yahoo.com>
2023-08-15 18:22:52 +0300

    sss

--- src/java/SuppliersAndStock/SuppliersAndStockController.java
+++ src/java/SuppliersAndStock/SuppliersAndStockController.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class SASController {
+public class SuppliersAndStockController {
 
     @RequestMapping(value = "suppliersAndStockDashboard")
     public String inventoryDashboard() {
