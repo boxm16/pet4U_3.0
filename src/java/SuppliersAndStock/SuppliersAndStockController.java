20e31c35f87ba9f3ef26569c6f5d4be26555e397
Michail Sitmalidis <boxm16@yahoo.com>
2023-08-20 11:22:55 +0300

    sss

--- src/java/SuppliersAndStock/SuppliersAndStockController.java
+++ src/java/SuppliersAndStock/SuppliersAndStockController.java
@@ -81,7 +81,7 @@ public class SuppliersAndStockController {
         SuppliersItem item = new SuppliersItem();
         item.setCode(soldItem.getCode());
         item.setDescription(soldItem.getDescription());
-        item.setEshopSales(0);
+        item.setEshopSales(soldItem.getEshopSales());
         item.setShopsSupply(soldItem.getShopsSupply());
         modelMap.addAttribute("supplier", supplier);
         modelMap.addAttribute("item", item);
