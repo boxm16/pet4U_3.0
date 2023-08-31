72d5e6dfe86548624e7f7a63b4ab7a748882e848
Michail Sitmalidis <boxm16@yahoo.com>
2023-08-31 22:49:37 +0300

    fix3

--- src/java/SuppliersAndStock/SuppliersAndStockController.java
+++ src/java/SuppliersAndStock/SuppliersAndStockController.java
@@ -228,7 +228,7 @@ public class SuppliersAndStockController {
             supplierItemsEntrySet.getValue().setQuantity(soldItem.getQuantity());
         }
 
-        model.addAttribute("items", supplierItems);
+        model.addAttribute("supplierItems", supplierItems);
         return "suppliersAndStock/orderMode";
     }
 
