b696a920d636186b6d2f5d668713ab4894b7a61c
Michail Sitmalidis <boxm16@yahoo.com>
2023-10-22 16:50:43 +0300

    sales working on

--- src/java/MonthSales/MonthSalesDao.java
+++ src/java/MonthSales/MonthSalesDao.java
@@ -127,7 +127,7 @@ public class MonthSalesDao {
         LocalDate startingDate = LocalDate.now();
         startingDate = startingDate.minusMonths(7);
 
-        String sql = "SELECT * FROM month_sales WHERE   date>" + startingDate + ";";
+        String sql = "SELECT * FROM month_sales WHERE code='"+itemCode+"'  date>" + startingDate + ";";
         Connection connection;
         Statement statement;
         ResultSet resultSet;
