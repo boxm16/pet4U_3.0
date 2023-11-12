0c55dabb75bc29d52af8844b559a50da9515ea85
Michail Sitmalidis <boxm16@yahoo.com>
2023-11-12 21:36:56 +0200

    starting wokring on eksagoges

--- src/java/SuppliersAndStock/SuppliersAndStockController.java
+++ src/java/SuppliersAndStock/SuppliersAndStockController.java
@@ -124,7 +124,7 @@ public class SuppliersAndStockController {
     @RequestMapping(value = "addItemToSupplier", method = RequestMethod.POST)
     public String addItemToSupplier(@RequestParam(name = "supplierId") String supplierId,
             @RequestParam(name = "code") String code,
-            @RequestParam(name = "minimalStock") String minimalStock,
+           
             @RequestParam(name = "orderUnit") String orderUnit,
             @RequestParam(name = "orderUnitCapacity") String orderUnitCapacity,
             @RequestParam(name = "note") String note,
@@ -136,7 +136,7 @@ public class SuppliersAndStockController {
         SoldItem soldItem = salesControllerX.getItemSales(code);
         SuppliersItem item = new SuppliersItem();
         item.setSupplierId(Integer.parseInt(supplierId));
-        item.setMinimalStock(Integer.parseInt(minimalStock));
+        item.setMinimalStock(0);
         item.setOrderUnit(orderUnit);
         item.setOrderUnitCapacity(Integer.parseInt(orderUnitCapacity));
         
@@ -145,7 +145,7 @@ public class SuppliersAndStockController {
         
      
         item.setNote(note);
-        if (minimalStock.isEmpty() || orderUnit.isEmpty() || orderUnitCapacity.isEmpty() || minimalStock.isEmpty()) {
+        if ( orderUnit.isEmpty() || orderUnitCapacity.isEmpty() ) {
             modelMap.addAttribute("resultColor", "rose");
             modelMap.addAttribute("result", "SOMETHING IS MISSING.");
             modelMap.addAttribute("supplier", supplier);
