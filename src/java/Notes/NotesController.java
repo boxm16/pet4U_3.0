88d8ad1e4c2fa2a0239ea58651e98789ea3b84e0
Michail Sitmalidis <boxm16@yahoo.com>
2023-11-27 22:29:04 +0200

    upload commit

--- src/java/Notes/NotesController.java
+++ src/java/Notes/NotesController.java
@@ -18,7 +18,7 @@ public class NotesController {
         modelMap.addAttribute("item", item);
         modelMap.addAttribute("altercode", altercode);
 
-        return "notes/noteServant";
+        return "notes/noteDisplay";
     }
     
      @RequestMapping(value = "saveNote", method = RequestMethod.POST)
