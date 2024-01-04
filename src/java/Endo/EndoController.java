4dd97e129fba1ea33173d5d986acbcb8f5842f61
Michail Sitmalidis <boxm16@yahoo.com>
2024-01-04 22:33:20 +0200

    next upload update

--- src/java/Endo/EndoController.java
+++ src/java/Endo/EndoController.java
@@ -18,42 +18,42 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EndoController {
-
+    
     @RequestMapping(value = "endoDashboard", method = RequestMethod.GET)
     public String endoDashboard(ModelMap modelMap) {
-
+        
         EndoDao endoDao = new EndoDao();
-
+        
         LinkedHashMap<String, Endo> incomingEndos = endoDao.getLastIncomingEndos(7);
         LinkedHashMap<String, Endo> receivingEndos = endoDao.getLastReceivingEndos(7);
-
+        
         modelMap.addAttribute("incomingEndos", incomingEndos);
         modelMap.addAttribute("receivingEndos", receivingEndos);
-
+        
         return "endo/endoDashboard";
-
+        
     }
-
+    
     @RequestMapping(value = "deltioApostolis", method = RequestMethod.GET)
     public String deltioApostolis(ModelMap modelMap) {
-
+        
         Pet4uItemsDao pet4uItemsDao = new Pet4uItemsDao();
         LinkedHashMap<String, Item> pet4UItemsRowByRow = pet4uItemsDao.getPet4UItemsRowByRow();
-
+        
         modelMap.addAttribute("pet4UItemsRowByRow", pet4UItemsRowByRow);
-
+        
         return "endo/deltioApostolis";
-
+        
     }
-
+    
     @RequestMapping(value = "/saveEndo", method = RequestMethod.POST)
     public String save(ModelMap modelMap, @ModelAttribute("endo") Endo endo) {
-
+        
         if (endo.getDateString() != null) {
             System.out.println("DA " + endo.getDateString());
             EndoDao endoDao = new EndoDao();
             String result = endoDao.saveDeltioApostolis(endo);
-
+            
         } else {
             System.out.println("Date String is NULL");
             modelMap.addAttribute("endo", endo);
@@ -63,50 +63,50 @@ public class EndoController {
         modelMap.addAttribute("endo", endo);
         return "endo/endoDashboard";
     }
-
+    
     @RequestMapping(value = "showDeltioApostolis", method = RequestMethod.GET)
     public String showDeltioApostolis(@RequestParam(name = "id") String id, ModelMap modelMap) {
         System.out.println(id);
-
+        
         TESTosteronDao testosteronDao = new TESTosteronDao();
         LinkedHashMap<String, Item> allPet4UItemsWithDeepSearch = testosteronDao.getAllPet4UItemsWithDeepSearch();
-
+        
         EndoDao endoDao = new EndoDao();
-
+        
         Endo endo = endoDao.getEndo(id, allPet4UItemsWithDeepSearch);
-
+        
         modelMap.addAttribute("endo", endo);
         return "endo/deltioApostolisDisplay";
     }
-
+    
     @RequestMapping(value = "compareEndo", method = RequestMethod.POST)
     public String compareEndo(@RequestParam(name = "endoIds") String endoIds, ModelMap modelMap) {
         System.out.println(endoIds);
-
+        
         ArrayList<String> endoIdsArray = createItemsIdsArray(endoIds);
-
+        
         EndoDao endoDao = new EndoDao();
         LinkedHashMap<String, DeliveryItem> sentItems = endoDao.getSentItems(endoIdsArray);
         LinkedHashMap<String, DeliveryItem> deliveredIetms = endoDao.getDeliveredItems();
-
+        
         Pet4uItemsDao inventoryDao = new Pet4uItemsDao();
         LinkedHashMap<String, Item> pet4UItemsRowByRow = inventoryDao.getPet4UItemsRowByRow();
-
+        
         System.out.println("SENT ITEMS SIZE: " + sentItems.size());
         System.out.println("DELIVERED ITEMS SIZE: " + deliveredIetms.size());
         DeliveryInvoice deliveryInvoice = new DeliveryInvoice();
         for (Map.Entry<String, DeliveryItem> deliveredIetmsEntry : deliveredIetms.entrySet()) {
             DeliveryItem deliveredItem = deliveredIetmsEntry.getValue();
-
+            
             String altercode = deliveredIetmsEntry.getKey();
-
+            
             DeliveryItem sentItem = sentItems.remove(altercode);
             Item itemWithDescription = pet4UItemsRowByRow.get(altercode);
-
+            
             if (itemWithDescription == null) {
-
+                
                 System.out.println("Pet4uItem  not present in the lists from microsoft db: altercode" + altercode);
-
+                
             } else {
                 if (sentItem == null) {
                     System.out.println("SENT ITEM IS NULL :" + altercode);
@@ -114,13 +114,13 @@ public class EndoController {
                     deliveredItem.setSentQuantity("0");
                     deliveredIetms.put(altercode, deliveredItem);
                 } else {
-
+                    
                     deliveredItem.setDescription(itemWithDescription.getDescription());
                     deliveredItem.setSentQuantity(sentItem.getSentQuantity());
                     deliveredIetms.put(altercode, deliveredItem);
                 }
             }
-
+            
         }
         if (sentItems.size() > 0) {
             System.out.println("LEFT OVERS: " + sentItems.size());
@@ -130,6 +130,7 @@ public class EndoController {
                 DeliveryItem di = sentItems.remove(key);
                 Item itemWithDescription = pet4UItemsRowByRow.get(key);
                 di.setDescription(itemWithDescription.getDescription());
+                di.setDeliveredQuantity("0");
                 deliveredIetms.put(key, di);
             }
         }
@@ -137,9 +138,9 @@ public class EndoController {
         System.out.println("delivered Items countL " + deliveredIetms.size());
         modelMap.addAttribute("deliveryInvoice", deliveryInvoice);
         return "endo/endoChecking";
-
+        
     }
-
+    
     private ArrayList<String> createItemsIdsArray(String inventoryItemsIds) {
         ArrayList idsArray = new ArrayList();
         //trimming and cleaning input
@@ -153,7 +154,7 @@ public class EndoController {
         String[] ids = inventoryItemsIds.split(",");
         idsArray.addAll(Arrays.asList(ids));
         return idsArray;
-
+        
     }
-
+    
 }
