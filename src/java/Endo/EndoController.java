40a415d3c55c583d145301566fec9c66ff30cd31
Michail Sitmalidis <boxm16@yahoo.com>
2024-01-06 17:33:25 +0200

    next upload update

--- src/java/Endo/EndoController.java
+++ src/java/Endo/EndoController.java
@@ -173,14 +173,14 @@ public class EndoController {
         LinkedHashMap<String, Endo> incomingEndos = endoDao.getLastIncomingEndos(7);
         LinkedHashMap<String, Endo> receivingEndos = endoDao.getLastReceivingEndos(7);
         LinkedHashMap<String, String> bindedEndos = endoDao.getAllBindedEndos();
-        System.out.println("BINDED ENDOS SIZE:"+bindedEndos.size());
 
         LinkedHashMap<String, BindedEndos> bindedEndosFiltered = new LinkedHashMap();
 
         for (Map.Entry<String, String> bindedEndosEntry : bindedEndos.entrySet()) {
             String bindedEndoId = bindedEndosEntry.getKey();
             String bindingEndoId = bindedEndosEntry.getValue();
-
+       
+            System.out.println("BINDED ENDO ID:"+bindedEndoId);
             if (receivingEndos.containsKey(bindingEndoId)) {
                 Endo bindedEndo = incomingEndos.remove(bindedEndoId);
                 if (bindedEndosFiltered.containsKey(bindingEndoId)) {
