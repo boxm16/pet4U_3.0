11c640e3ac0793ef6d0fa945b3192de7c3203f85
Michail Sitmalidis <boxm16@yahoo.com>
2024-01-06 10:14:10 +0200

    next upload update

--- src/java/Endo/EndoDao.java
+++ src/java/Endo/EndoDao.java
@@ -135,7 +135,7 @@ public class EndoDao {
         nowDate = nowDate.minusDays(days);
         System.out.println("NOW DATE: "+nowDate);
         LinkedHashMap<String, Endo> endoInvoices = new LinkedHashMap();
-        String sql = "SELECT DISTINCT  [DOCID], [DOCNUMBER],[DOCDATE], [FROM_WH] FROM  [petworld].[dbo].[WH_ENDA]  WHERE where [DOCDATE] >= '" + nowDate + "';";
+        String sql = "SELECT DISTINCT  [DOCID], [DOCNUMBER],[DOCDATE], [FROM_WH] FROM  [petworld].[dbo].[WH_ENDA]  WHERE  [DOCDATE] >= '" + nowDate + "';";
         Connection connection;
         Statement statement;
         ResultSet resultSet;
