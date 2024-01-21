bde71b10b23d321368268c28872348476abc6447
Michail Sitmalidis <boxm16@yahoo.com>
2024-01-21 13:31:44 +0200

    upload commit

--- src/java/Endo/EndoController.java
+++ src/java/Endo/EndoController.java
@@ -79,26 +79,28 @@ public class EndoController {
             DeliveryItem sentItem = sentItems.remove(altercode);
             Item itemWithDescription = pet4UItemsRowByRow.get(altercode);
 
-            if (itemWithDescription == null) {
-
-                System.out.println("Pet4uItem  not present in the lists from microsoft db: altercode" + altercode);
-
-            } else {
-                if (sentItem == null) {
-                    System.out.println("SENT ITEM IS NULL :" + altercode);
-                    deliveredItem.setDescription(itemWithDescription.getDescription());
-                    deliveredItem.setSentQuantity("0");
-                    deliveredIetms.put(altercode, deliveredItem);
+            if (sentItem == null) {
+                System.out.println("SENT ITEM IS NULL :" + altercode);
+                if (itemWithDescription == null) {
+                    deliveredItem.setDescription("NO DATA FOR THIS CODE");
                 } else {
-
                     deliveredItem.setDescription(itemWithDescription.getDescription());
-                    deliveredItem.setSentQuantity(sentItem.getSentQuantity());
-                    deliveredIetms.put(altercode, deliveredItem);
                 }
+                deliveredItem.setSentQuantity("0");
+                deliveredIetms.put(altercode, deliveredItem);
+            } else {
+                if (itemWithDescription == null) {
+                    deliveredItem.setDescription("NO DATA FOR THIS CODE");
+                } else {
+                    deliveredItem.setDescription(itemWithDescription.getDescription());
+                }
+                deliveredItem.setSentQuantity(sentItem.getSentQuantity());
+                deliveredIetms.put(altercode, deliveredItem);
             }
-
         }
-        if (sentItems.size() > 0) {
+
+        if (sentItems.size()
+                > 0) {
 
             System.out.println("LEFT OVERS: " + sentItems.size());
             for (Map.Entry<String, DeliveryItem> sentItemsEntry : sentItems.entrySet()) {
@@ -118,9 +120,12 @@ public class EndoController {
             }
 
         }
+
         deliveryInvoice.setItems(deliveredIetms);
 
-        modelMap.addAttribute("deliveryInvoice", deliveryInvoice);
+        modelMap.addAttribute(
+                "deliveryInvoice", deliveryInvoice);
+
         return "endo/endoChecking";
 
     }
