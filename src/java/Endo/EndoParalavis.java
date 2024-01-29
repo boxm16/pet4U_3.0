ebbaadc8c452b8bc50c0ce55707127976bcb318f
Michail Sitmalidis <boxm16@yahoo.com>
2024-01-29 21:10:14 +0200

    upload commit

--- src/java/Endo/EndoParalavis.java
+++ src/java/Endo/EndoParalavis.java
@@ -71,7 +71,7 @@ public class EndoParalavis {
         this.items = items;
     }
 
-    public List getNumberAsList() {
+    public List getNumberAsArrayList() {
         return numberAsArrayList;
     }
 
