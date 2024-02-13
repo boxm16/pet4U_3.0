350dd753c3a63edb5b7b15c01c8be62b5dfdfa68
Michail Sitmalidis <boxm16@yahoo.com>
2024-02-13 21:28:50 +0200

    upload commit

--- src/java/Endo/EndoDaoX.java
+++ src/java/Endo/EndoDaoX.java
@@ -419,7 +419,15 @@ public class EndoDaoX {
                 EndoOrder endoOrder = new EndoOrder();
 
                 endoOrder.setId(resultSet.getString("id"));
-                endoOrder.setDestination(resultSet.getString("destination"));
+
+                String destination = resultSet.getString("destination");
+                if (destination.equals("ΑΓ_ΠΑΡΑΣΚΕΥΗ")) {
+                    destination = "ΑΓ. ΠΑΡΑΣΚΕΥΗ";
+                }
+                if (destination.equals("MΕΝΙΔΙ")) {
+                    destination = "ΜΕΝΙΔΙ";
+                }
+                endoOrder.setDestination(resultSet.getString(destination));
                 endoOrder.setNote(resultSet.getString("note"));
                 endoOrders.put(resultSet.getString("id"), endoOrder);
             }
@@ -454,7 +462,7 @@ public class EndoDaoX {
                     if (destination.equals("ΑΓ_ΠΑΡΑΣΚΕΥΗ")) {
                         destination = "ΑΓ. ΠΑΡΑΣΚΕΥΗ";
                     }
-                      if (destination.equals("MΕΝΙΔΙ")) {
+                    if (destination.equals("MΕΝΙΔΙ")) {
                         destination = "ΜΕΝΙΔΙ";
                     }
                     endoOrder.setDestination(resultSet.getString(destination));
