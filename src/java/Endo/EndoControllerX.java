6cce45b7e4a5700b59785d7e3f0288564b862b76
Michail Sitmalidis <boxm16@yahoo.com>
2024-02-14 21:03:22 +0200

    upload commit

--- src/java/Endo/EndoControllerX.java
+++ src/java/Endo/EndoControllerX.java
@@ -263,7 +263,7 @@ public class EndoControllerX {
     @RequestMapping(value = "endoApostoles")
     public String endoApostoles(ModelMap model) {
         EndoDaoX endoDaoX = new EndoDaoX();
-        String date = "2024-02-07";
+        String date = "2024-02-06";
         String date1 = "2024-02-07";
         LinkedHashMap<String, EndoOrder> endoOrdersTitles = endoDaoX.getEndoOrdersTitles(date);
         LinkedHashMap<String, EndoApostolis> outgoingDeltioApostolisTitles = endoDaoX.getOutgoingDeltioApostolisTitles(date1);
