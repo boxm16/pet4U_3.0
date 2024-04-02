f2ede95d4efcfd30b0c0791c291ac24bd0fb531e
Michail Sitmalidis <boxm16@yahoo.com>
2024-04-03 00:14:27 +0300

    encryption added

---
+++ src/java/Encryption/EncryptionController.java
@@ -0,0 +1,17 @@
+
+package Encryption;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class EncryptionController {
+     @RequestMapping(value = "encryptionIndex", method = RequestMethod.GET)
+    public String deltioApostolis() {
+
+      
+        return "redirect:index.htm";
+
+    }
+}
