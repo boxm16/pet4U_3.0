ae370147a0634c666057ea6f950f9c9dcb4e5acd
Michail Sitmalidis <boxm16@yahoo.com>
2024-04-07 13:58:12 +0300

    up

--- src/java/CamelotSales/CamelotSalesDao.java
+++ src/java/CamelotSales/CamelotSalesDao.java
@@ -6,7 +6,13 @@
 package CamelotSales;
 
 import SalesX.SoldItem;
+import Service.DatabaseConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,10 +21,42 @@ import java.util.ArrayList;
 public class CamelotSalesDao {
 
     String insertNewUpload(String date, ArrayList<SoldItem> soldItems) {
-        for (SoldItem soldItem : soldItems) {
-            System.out.println(soldItem.getCode() + "-" + soldItem.getEshopSales());
+        System.out.println("STARTING INSERTING UPLOADED DATA");
+        System.out.println(" STARTING ADDING ITEMS TO 'camelot_month_sales' INSERTION BATCH");
+        try {
+            DatabaseConnectionFactory databaseConnectionFactory = new DatabaseConnectionFactory();
+            Connection connection = databaseConnectionFactory.getMySQLConnection();
+
+            connection.setAutoCommit(false);
+            PreparedStatement itemInsertStatement = connection.prepareStatement("INSERT INTO camelot_month_sales (code, date, sales) VALUES (?,?,?)");
+            int index = 0;
+            for (SoldItem soldItem : soldItems) {
+
+                itemInsertStatement.setString(1, soldItem.getCode());
+                itemInsertStatement.setString(2, date);
+                itemInsertStatement.setDouble(3, soldItem.getEshopSales());
+
+                itemInsertStatement.addBatch();
+                index++;
+                if (index % 1000 == 0) {
+                    System.out.println(index + " soldItems added to batch");
+                }
+
+            }
+
+            itemInsertStatement.executeBatch();
+            connection.commit();
+            itemInsertStatement.close();
+
+            connection.close();
+
+            System.out.println("Camelot 'sales' INSERTION BATCH EXECUTED.");
+            System.out.println("-----------------------------------");
+        } catch (SQLException ex) {
+            Logger.getLogger(CamelotSalesDao.class.getName()).log(Level.SEVERE, null, ex);
+            return ex.getMessage();
         }
-        return "done";
+        return "CAMELOT SALES UPLOAD  EXECUTED SUCCESSFULLY.";
     }
 
 }
