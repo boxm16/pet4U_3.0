c563e5dd155fcd96245aa347a2eb6872d17bd24b
Michail Sitmalidis <boxm16@yahoo.com>
2024-04-15 02:45:23 +0300

    upload commit

--- src/java/MonthSales/MonthSalesDao.java
+++ src/java/MonthSales/MonthSalesDao.java
@@ -266,7 +266,7 @@ public class MonthSalesDao {
 
     public LinkedHashMap<String, Double> getLast30DaysSales(String itemCode) {
         LocalDate date = LocalDate.now();
-        LocalDate firstDate = date.minusDays(30);
+        LocalDate firstDate = date.minusDays(29);
         LocalDate lastDate = date.minusDays(1);
 
         LinkedHashMap<String, Double> daysSales = new LinkedHashMap<>();
