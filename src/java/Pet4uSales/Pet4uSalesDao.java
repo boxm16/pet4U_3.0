f0cc52c5852a89fa6880ba8b7f20a4fe5a7a60c2
Michail Sitmalidis <boxm16@yahoo.com>
2024-05-06 16:29:15 +0300

    upload commit

--- src/java/Pet4uSales/Pet4uSalesDao.java
+++ src/java/Pet4uSales/Pet4uSalesDao.java
@@ -69,7 +69,7 @@ public class Pet4uSalesDao {
         return items;
     }
 
-    LinkedHashMap<String, SoldItem> getPet4uMonthSalesFromMicrosoftDB(String dateString, LinkedHashMap<String, SoldItem> camelotAllItemsForSales) {
+    LinkedHashMap<String, SoldItem> getPet4uMonthSalesFromMicrosoftDB(String dateString, LinkedHashMap<String, SoldItem> pet4uAllItemsForSales) {
         LocalDate date = LocalDate.parse(dateString);
         LocalDate firstDate = date.with(TemporalAdjusters.firstDayOfMonth());
         LocalDate lastDate = date.with(TemporalAdjusters.lastDayOfMonth());
@@ -79,25 +79,45 @@ public class Pet4uSalesDao {
 
         try {
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT ABBREVIATION, SUM(QTY) AS SALES"
+            ResultSet resultSetSales = statement.executeQuery("SELECT ABBREVIATION, SUM(QTY) AS SALES"
                     + "  FROM [petworld].[dbo].[WH_SALES_VAR] WHERE ENTRYDATE >= '" + firstDate + "' "
                     + "AND ENTRYDATE <= '" + lastDate + "' group by ABBREVIATION order by ABBREVIATION;");
 
-            while (resultSet.next()) {
-                String code = resultSet.getString("ABBREVIATION").trim();
-                SoldItem soldItem = camelotAllItemsForSales.get(code);
+            while (resultSetSales.next()) {
+                String code = resultSetSales.getString("ABBREVIATION").trim();
+                SoldItem soldItem = pet4uAllItemsForSales.get(code);
+                if (soldItem == null) {
+                    System.out.println("Pet4u Month Sales Upload: Sales exist, itemCode doesnt exist. code:" + code);
+                    soldItem = new SoldItem();
+                    soldItem.setCode(code);
+                }
+                double sales = resultSetSales.getDouble("SALES");
+                //   System.out.println(code+" "+sales);
+                soldItem.setEshopSales(sales);
+                pet4uAllItemsForSales.put(code, soldItem);
+            }
+
+            Statement statementEndo = connection.createStatement();
+            ResultSet resultSetSalesEndo = statementEndo.executeQuery("SELECT ABBREVIATION, SUM(QTY) AS SENT"
+                    + "  FROM [petworld].[dbo].[WH_ENDA_VAR] WHERE ENTRYDATE >= '" + firstDate + "' "
+                    + "AND ENTRYDATE <= '" + lastDate + "' group by ABBREVIATION order by ABBREVIATION;");
+            while (resultSetSalesEndo.next()) {
+                String code = resultSetSales.getString("ABBREVIATION").trim();
+                SoldItem soldItem = pet4uAllItemsForSales.get(code);
                 if (soldItem == null) {
                     soldItem = new SoldItem();
                     soldItem.setCode(code);
                 }
-                double sales = resultSet.getDouble("SALES");
+                double sent = resultSetSalesEndo.getDouble("SENT");
                 //   System.out.println(code+" "+sales);
-                soldItem.setEshopSales(sales);
-                camelotAllItemsForSales.put(code, soldItem);
+                soldItem.setShopsSupply(sent);
+                pet4uAllItemsForSales.put(code, soldItem);
             }
-
-            resultSet.close();
+            resultSetSales.close();
+            resultSetSalesEndo.close();
             statement.close();
+            statementEndo.close();
+
             connection.close();
         } catch (SQLException ex) {
             Logger.getLogger(Pet4uSalesDao.class.getName()).log(Level.SEVERE, null, ex);
