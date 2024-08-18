5cabefb8dfb7774238605603bd549f8053b25ab3
Michail Sitmalidis <boxm16@yahoo.com>
2024-08-18 23:10:19 +0300

    orders analysis ........

--- src/java/Order/OrderController.java
+++ src/java/Order/OrderController.java
@@ -48,6 +48,7 @@ public class OrderController {
             LocalDateTime creationDateTime = orderEntry.getValue().getCreationDateTime();
             int hour = creationDateTime.getHour();
             Integer q = ordersTimeStrucuterOfDate.get(hour);
+            System.out.println("q: "+q);
             q++;
             ordersTimeStrucuterOfDate.put(hour, q);
         }
