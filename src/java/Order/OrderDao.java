c4eec81278915a25eba2b2c38d72193cdbd9195f
Michail Sitmalidis <boxm16@yahoo.com>
2024-08-19 23:52:53 +0300

    orders analysis ........

--- src/java/Order/OrderDao.java
+++ src/java/Order/OrderDao.java
@@ -511,7 +511,7 @@ public class OrderDao {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT DISTINCT DOCID , ENTRYDATE , DOCNAME "
                     + "  FROM [petworld].[dbo].[WH_SALES_DOCS] WHERE (DOCNAME='ΚΑΠΔ' OR DOCNAME= 'ΚΔΑΤ1' OR DOCNAME= 'ΚΑΕΛ' OR DOCNAME= 'ΚΠΤΔ1' ) "
-                    + "AND DATE_TIME between  '2023-12-31' AND '2025-01-01' order by DOCID  ;");
+                    + "AND DATE_TIME >=  '2024-01-01 00:00:00.000 ' AND DATE_TIME <= '2024-12-31 23:59:59.999' order by DOCID  ;");
 
             while (resultSet.next()) {
                 String dateTimeStampString = resultSet.getString("ENTRYDATE");
