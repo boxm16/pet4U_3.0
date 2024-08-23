d79d3f1b1b4d494110fa35d451d49f00ee7b56da
Michail Sitmalidis <boxm16@yahoo.com>
2024-08-23 21:33:44 +0300

    orders analysis ........

--- src/java/Order/PositioningController.java
+++ src/java/Order/PositioningController.java
@@ -1,6 +1,5 @@
 package Order;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PositioningController {
 
-    @Autowired
-    private PositioningDao positioningDao;
+   
 
     @RequestMapping(value = "positioning")
     public String orderDashboard(ModelMap modelMap) {
