9d0ce924215cede5a59d871ffc01210f6f2db6e2
Michail Sitmalidis <boxm16@yahoo.com>
2024-08-24 00:28:39 +0300

    orders analysis ........

--- src/java/Order/OrderController.java
+++ src/java/Order/OrderController.java
@@ -381,14 +381,15 @@ public class OrderController {
     }
 
     @RequestMapping(value = "getAllDocsForItemBetweenTwoDatesWithThisBlockPosition")
-    public String getAllDocsForItemBetweenTwoDatesWithThisBlockPosition(@RequestParam(name = "blockPosition") String position, @RequestParam(name = "itemCode") String itemCode, @RequestParam(name = "startDate") String startDate, @RequestParam(name = "endDate") String endDate, ModelMap modelMap) {
+    public String getAllDocsForItemBetweenTwoDatesWithThisBlockPosition(@RequestParam(name = "blockPosition") String blockPosition, @RequestParam(name = "itemCode") String itemCode, @RequestParam(name = "startDate") String startDate, @RequestParam(name = "endDate") String endDate, ModelMap modelMap) {
         LinkedHashMap<Integer, Order> orders0 = orderDao.getAllDocs(startDate, endDate);
         LinkedHashMap<Integer, Order> orders = new LinkedHashMap<Integer, Order>();
         for (Map.Entry<Integer, Order> ordersEntry : orders0.entrySet()) {
             LinkedHashMap<String, Item> items = ordersEntry.getValue().getItems();
             for (Map.Entry<String, Item> itemsEntry : items.entrySet()) {
                 Item item = itemsEntry.getValue();
-                if (item.getCode().equals("itemCode") && item.getPosition().contains(position)) {
+                if (item.getCode().equals("itemCode") && item.getPosition().contains(blockPosition)) {
+                    System.out.println("UARRA");
                 orders.put(ordersEntry.getKey(), ordersEntry.getValue());
                 }
             }
