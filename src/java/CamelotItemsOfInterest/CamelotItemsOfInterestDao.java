f1271e961025d2cea6e93c7e3682ef1a4183cbde
Michail Sitmalidis <boxm16@yahoo.com>
2024-11-22 22:22:53 +0200

    Fixing new columns in database, disabled, and so on

--- src/java/CamelotItemsOfInterest/CamelotItemsOfInterestDao.java
+++ src/java/CamelotItemsOfInterest/CamelotItemsOfInterestDao.java
@@ -537,7 +537,7 @@ public class CamelotItemsOfInterestDao {
                         position_2 = resultSet.getString("EXPR2").trim();
                     }
                     item.setPosition(position_1 + position_2);
-                    item.setQuantity(resultSet.getString("QTYBALANCE").trim());
+                    item.setQuantity(resultSet.getString("QTYBALANCE"));
                     items.put(code, item);
                 }
                 AltercodeContainer altercodeContainer = new AltercodeContainer();
