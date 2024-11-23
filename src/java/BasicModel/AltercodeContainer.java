d9755ed9618ec011523f0a574066a2769ec01929
Michail Sitmalidis <boxm16@yahoo.com>
2024-11-23 19:56:03 +0200

    Fixing new columns in database, disabled, and so on

--- src/java/BasicModel/AltercodeContainer.java
+++ src/java/BasicModel/AltercodeContainer.java
@@ -5,6 +5,8 @@ public class AltercodeContainer {
     private String altercode;
     private String status;
     private boolean mainBarcode;
+    private boolean packageBarcode;
+    private double itemsInPackage;
 
     public String getAltercode() {
         return altercode;
@@ -28,6 +30,22 @@ public class AltercodeContainer {
 
     public void setMainBarcode(boolean mainBarcode) {
         this.mainBarcode = mainBarcode;
+    }
+
+    public boolean isPackageBarcode() {
+        return packageBarcode;
+    }
+
+    public void setPackageBarcode(boolean packageBarcode) {
+        this.packageBarcode = packageBarcode;
+    }
+
+    public double getItemsInPackage() {
+        return itemsInPackage;
+    }
+
+    public void setItemsInPackage(double itemsInPackage) {
+        this.itemsInPackage = itemsInPackage;
     }
 
 }
--- src/java/Search/SearchDao.java
+++ src/java/Search/SearchDao.java
@@ -17,10 +17,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 
 public class SearchDao {
-
+    
     @Autowired
     private DatabaseConnectionFactory databaseConnectionFactory;
-
+    
     public Item getItemByAltercode(String altercode) {
         DatabaseConnectionFactory databaseConnectionFactory = new DatabaseConnectionFactory();
         Connection connection = databaseConnectionFactory.getPet4UMicrosoftSQLConnection();
@@ -70,6 +70,13 @@ public class SearchDao {
                         altercodeContainer.setMainBarcode(false);
                     }
                 }
+                
+                if (resultSet.getShort("IS_PACK_BC") == 0) {
+                    //do nothing
+                } else {
+                    altercodeContainer.setPackageBarcode(true);
+                    altercodeContainer.setItemsInPackage(resultSet.getDouble("PACK_QTY"));
+                }
                 if (item != null) {//It should never be, i mean, if there is an altercode, there is an item. But, just in any case
                     item.addAltercodeContainer(altercodeContainer);
                 }
@@ -82,23 +89,23 @@ public class SearchDao {
         }
         return item;
     }
-
+    
     public LinkedHashMap<String, Item> getItemsByAltercodeMask(String altercodeMask) {
-
+        
         LinkedHashMap<String, Item> items = new LinkedHashMap<>();
-
+        
         Connection connection;
         Statement statement;
         ResultSet resultSet;
-
+        
         try {
             connection = databaseConnectionFactory.getPet4UMicrosoftSQLConnection();
-
+            
             statement = connection.createStatement();
 
 //maybe thre is some join, or other clause for things like this, but i dnt know yet, no time for search
             String sql = "SELECT ABBREVIATION FROM WH1 WHERE ALTERNATECODE LIKE '" + altercodeMask + "'";
-
+            
             resultSet = statement.executeQuery(sql);
             ArrayList<String> itemCodes = new ArrayList<>();
             String code;
@@ -109,7 +116,7 @@ public class SearchDao {
                     itemCodes.add(code);
                 }
             }
-
+            
             if (itemCodes.isEmpty()) {
                 statement.close();
                 connection.close();
@@ -118,13 +125,13 @@ public class SearchDao {
             StringBuilder query = new StringBuilder();
             StringBuilder queryBuilderInitialPart = new StringBuilder("SELECT * FROM WH1  WHERE ABBREVIATION IN");
             StringBuilder queryBuilderDateStampPart = buildStringFromArrayList(itemCodes);
-
+            
             query = queryBuilderInitialPart.append(queryBuilderDateStampPart);
-
+            
             System.out.println(query);
             statement = connection.createStatement();
             resultSet = statement.executeQuery(query.toString());
-
+            
             while (resultSet.next()) {
                 code = resultSet.getString("ABBREVIATION").trim();
                 String altercode = resultSet.getString("ALTERNATECODE").trim();
@@ -134,7 +141,7 @@ public class SearchDao {
                 } else {
                     altercodeStatus = resultSet.getString("CODEDESCRIPTION").trim();
                 }
-
+                
                 String description = resultSet.getString("NAME").trim();
                 String position = resultSet.getString("EXPR1");
                 String quantity = resultSet.getString("QTYBALANCE");
@@ -142,26 +149,26 @@ public class SearchDao {
                 if (resultSet.getString("EXPR2") != null) {
                     state = resultSet.getString("EXPR2");
                 }
-
+                
                 if (items.containsKey(code)) {
                     Item item = items.get(code);
-
+                    
                     AltercodeContainer altercodeContainer = new AltercodeContainer();
                     altercodeContainer.setAltercode(altercode);
                     altercodeContainer.setStatus(altercodeStatus);
                     item.addAltercodeContainer(altercodeContainer);
-
+                    
                     items.put(code, item);
                 } else {
                     Item item = new Item();
-
+                    
                     item.setCode(code);
-
+                    
                     AltercodeContainer altercodeContainer = new AltercodeContainer();
                     altercodeContainer.setAltercode(altercode);
                     altercodeContainer.setStatus(altercodeStatus);
                     item.addAltercodeContainer(altercodeContainer);
-
+                    
                     item.setDescription(description);
                     item.setPosition(position);
                     item.setQuantity(quantity);
@@ -169,31 +176,31 @@ public class SearchDao {
                     items.put(code, item);
                 }
             }
-
+            
             statement.close();
             connection.close();
-
+            
         } catch (SQLException ex) {
             Logger.getLogger(SearchDao.class.getName()).log(Level.SEVERE, null, ex);
         }
         return items;
     }
-
+    
     public LinkedHashMap<String, Item> getItemsByDescriptionMask(String descriptionMask) {
-
+        
         LinkedHashMap<String, Item> items = new LinkedHashMap<>();
         String sql = "SELECT * FROM WH1 WHERE NAME LIKE '" + descriptionMask + "'";
         System.out.println(sql);
         Connection connection;
         Statement statement;
         ResultSet resultSet;
-
+        
         try {
             connection = databaseConnectionFactory.getPet4UMicrosoftSQLConnection();
             statement = connection.createStatement();
-
+            
             resultSet = statement.executeQuery(sql);
-
+            
             while (resultSet.next()) {
                 String code = resultSet.getString("ABBREVIATION").trim();
                 String altercode = resultSet.getString("ALTERNATECODE").trim();
@@ -210,26 +217,26 @@ public class SearchDao {
                 if (resultSet.getString("EXPR2") != null) {
                     state = resultSet.getString("EXPR2");
                 }
-
+                
                 if (items.containsKey(code)) {
                     Item item = items.get(code);
-
+                    
                     item.setCode(code);
-
+                    
                     AltercodeContainer altercodeContainer = new AltercodeContainer();
                     altercodeContainer.setAltercode(altercode);
                     altercodeContainer.setStatus(altercodeStatus);
                     item.addAltercodeContainer(altercodeContainer);
-
+                    
                     items.put(code, item);
                 } else {
                     Item item = new Item();
-
+                    
                     AltercodeContainer altercodeContainer = new AltercodeContainer();
                     altercodeContainer.setAltercode(altercode);
                     altercodeContainer.setStatus(altercodeStatus);
                     item.addAltercodeContainer(altercodeContainer);
-
+                    
                     item.setDescription(description);
                     item.setPosition(position);
                     item.setQuantity(quantity);
@@ -237,18 +244,18 @@ public class SearchDao {
                     items.put(code, item);
                 }
             }
-
+            
             statement.close();
             connection.close();
-
+            
         } catch (SQLException ex) {
             Logger.getLogger(SearchDao.class.getName()).log(Level.SEVERE, null, ex);
         }
         return items;
     }
-
+    
     private StringBuilder buildStringFromArrayList(ArrayList<String> arrayList) {
-
+        
         StringBuilder stringBuilder = new StringBuilder("(");
         if (arrayList.isEmpty()) {
             stringBuilder.append(")");
@@ -268,18 +275,18 @@ public class SearchDao {
         }
         return stringBuilder;
     }
-
+    
     public LinkedHashMap<String, Item> deepSearchForAltercodeMask(String altercodeMask) {
-
+        
         LinkedHashMap<String, Item> items = new LinkedHashMap<>();
-
+        
         Connection connection;
         Statement statement;
         ResultSet resultSet;
-
+        
         try {
             connection = databaseConnectionFactory.getPet4UMicrosoftSQLConnection();
-
+            
             statement = connection.createStatement();
 
 //maybe thre is some join, or other clause for things like this, but i dnt know yet, no time for search
@@ -302,10 +309,10 @@ public class SearchDao {
                     + "AND [petworld].[EliteUser].[ICV].WARID=11 "
                     + " AND ALTERNATECODE LIKE '" + altercodeMask + "'"
                     + "ORDER BY EXPR1;";
-
+            
             statement = connection.createStatement();
             resultSet = statement.executeQuery(sql);
-
+            
             while (resultSet.next()) {
                 String code = resultSet.getString("ABBREVIATION").trim();
                 String altercode = resultSet.getString("ALTERNATECODE").trim();
@@ -315,7 +322,7 @@ public class SearchDao {
                 } else {
                     altercodeStatus = resultSet.getString("CODEDESCRIPTION").trim();
                 }
-
+                
                 String description = resultSet.getString("NAME").trim();
                 String position = resultSet.getString("EXPR1");
                 String quantity = resultSet.getString("QTYBALANCE");
@@ -323,26 +330,26 @@ public class SearchDao {
                 if (resultSet.getString("EXPR2") != null) {
                     state = resultSet.getString("EXPR2");
                 }
-
+                
                 if (items.containsKey(code)) {
                     Item item = items.get(code);
-
+                    
                     item.setCode(code);
-
+                    
                     AltercodeContainer altercodeContainer = new AltercodeContainer();
                     altercodeContainer.setAltercode(altercode);
                     altercodeContainer.setStatus(altercodeStatus);
                     item.addAltercodeContainer(altercodeContainer);
-
+                    
                     items.put(code, item);
                 } else {
                     Item item = new Item();
-
+                    
                     AltercodeContainer altercodeContainer = new AltercodeContainer();
                     altercodeContainer.setAltercode(altercode);
                     altercodeContainer.setStatus(altercodeStatus);
                     item.addAltercodeContainer(altercodeContainer);
-
+                    
                     item.setDescription(description);
                     item.setPosition(position);
                     item.setQuantity(quantity);
@@ -350,14 +357,14 @@ public class SearchDao {
                     items.put(code, item);
                 }
             }
-
+            
             statement.close();
             connection.close();
-
+            
         } catch (SQLException ex) {
             Logger.getLogger(SearchDao.class.getName()).log(Level.SEVERE, null, ex);
         }
         return items;
     }
-
+    
 }
