38962b63597d206a6b9527e7a649e0febb43df64
Michail Sitmalidis <boxm16@yahoo.com>
2024-11-24 23:09:04 +0200

    Fixing new columns in database, disabled, and so on

--- src/java/CamelotSearch/CamelotSearchController.java
+++ src/java/CamelotSearch/CamelotSearchController.java
@@ -1071,7 +1071,11 @@ public class CamelotSearchController {
 
         barcodePrinter.setCode(item.getCode());
         //  barcodePrinter.setBarcode(altercode.substring(altercode.length() - 6));
-        barcodePrinter.setBarcode(altercode.substring(altercode.length() - 6));
+        if (altercode.length() >= 6) {
+            barcodePrinter.setBarcode(altercode.substring(altercode.length() - 6));
+        } else {
+            barcodePrinter.setBarcode(altercode);
+        }
         barcodePrinter.setDescription(item.getDescription());
         // String position = item.getPosition().substring(2);
         barcodePrinter.setPosition(item.getPosition());
@@ -1080,6 +1084,113 @@ public class CamelotSearchController {
 
         return "index";
     }
+
+    //++
+    @RequestMapping(value = "printCamelotMainBarcode")
+    public String printCamelotMainBarcode(@RequestParam(name = "altercode") String altercode, ModelMap model) {
+        System.out.println("Altercode :" + altercode);
+        CamelotSearchDao searchDao = new CamelotSearchDao();
+        Item item = searchDao.getItemByAltercode(altercode);
+
+        if (item == null) {
+            System.out.println("Item Null");
+            model.addAttribute("message", "Can't print this label. Item is NULL. Ask for help");
+            return "errorPage";
+        }
+        String mainBarcode = item.getMainBarcode();
+
+        if (mainBarcode == null) {
+            System.out.println("mainBarcode Null");
+            model.addAttribute("message", "Can't print this label. mainBarcode is NULL. Ask for help");
+            return "errorPage";
+        }
+        OutputStream out = null;
+        try {
+
+            Code128Bean barcode128Bean = new Code128Bean();
+            barcode128Bean.setCodeset(Code128Constants.CODESET_B);
+            final int dpi = 100;
+            //Configure the barcode generator
+            //adjust barcode width here
+            barcode128Bean.setModuleWidth(UnitConv.in2mm(5.0f / dpi));
+            barcode128Bean.doQuietZone(false);
+            barcode128Bean.setBarHeight(8);
+            //bean.setVerticalQuietZone(3);
+            barcode128Bean.setQuietZone(0);
+            barcode128Bean.setMsgPosition(HumanReadablePlacement.HRP_NONE);
+            //Open output file
+            File outputFile = new File("C:/Pet4U_3.0/barcode.png");
+            out = new FileOutputStream(outputFile);
+            try {
+                BitmapCanvasProvider canvasProvider = new BitmapCanvasProvider(
+                        out, "image/x-png", dpi, BufferedImage.TYPE_BYTE_BINARY, false, 0);
+
+                barcode128Bean.generateBarcode(canvasProvider, mainBarcode);
+
+                try {
+                    canvasProvider.finish();
+                } catch (IOException ex) {
+                    Logger.getLogger(Pet4uItemsController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } finally {
+                try {
+                    out.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(Pet4uItemsController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+            // model.addAttribute("code", code);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Pet4uItemsController.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                out.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Pet4uItemsController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        //+++++++++++++++++++++++
+        String path = "C:/Pet4U_3.0/qrCode.png";
+        String charset = "UTF-8";
+        Map<EncodeHintType, ErrorCorrectionLevel> hashMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
+//generates QR code with Low level(L) error correction capability
+        hashMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+        try {
+            //invoking the user-defined method that creates the QR code
+            generateQRcode(altercode, path, charset, hashMap, 200, 200);//increase or decrease height and width accodingly
+//prints if the QR code is generated
+        } catch (WriterException ex) {
+            Logger.getLogger(Pet4uItemsController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Pet4uItemsController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+//-------------
+        String printName = "\\\\eshoplaptop\\ZDesigner GC420t (EPL) (Αντιγραφή 1)";
+        CamelotBarcodePrinter barcodePrinter = new CamelotBarcodePrinter();
+        // String printName = "ZDesigner GC420t (EPL)";
+        // BarcodePrinter2 barcodePrinter = new BarcodePrinter2();
+//---------------
+        barcodePrinter.setLabelsCount(1);
+
+        barcodePrinter.setCode(item.getCode());
+        //  barcodePrinter.setBarcode(altercode.substring(altercode.length() - 6));
+        if (mainBarcode.length() >= 6) {
+            barcodePrinter.setBarcode(mainBarcode.substring(altercode.length() - 6));
+        } else {
+            barcodePrinter.setBarcode(mainBarcode);
+        }
+
+        barcodePrinter.setDescription(item.getDescription());
+        // String position = item.getPosition().substring(2);
+        barcodePrinter.setPosition(item.getPosition());
+
+        barcodePrinter.printSomething(printName);
+
+        return "index";
+    }
+    //-__
 
     public static void generateQRcode(String data, String path, String charset, Map map, int h, int w) throws WriterException, IOException {
 //the BitMatrix class represents the 2D matrix of bits  
