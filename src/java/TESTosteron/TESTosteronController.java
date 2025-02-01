05a56423002414a59cc95f4be4d26efc886bf4f5
Michail Sitmalidis <boxm16@yahoo.com>
2025-02-01 20:56:25 +0200

    endo delivery starting

--- src/java/TESTosteron/TESTosteronController.java
+++ src/java/TESTosteron/TESTosteronController.java
@@ -281,7 +281,7 @@ public class TESTosteronController {
             String newPickLocation = "A27";
 
             // API URL (SAP Business One Service Layer)
-            String apiUrl = "https://192.168.0.183:50000/b1s/v2/sml.svc/Items('" + itemCode + "')";
+            String apiUrl = "https://192.168.0.183:50000/b1s/v2/Items('" + itemCode + "')";
 
             // Prepare JSON body
             String jsonBody = "{ \"U_PickLocation\": \"" + newPickLocation + "\" }";
@@ -318,10 +318,9 @@ public class TESTosteronController {
 
             // Set headers
             conn.setRequestProperty("Content-Type", "application/json");
-
-            //   conn.setRequestProperty("Authorization", "Basic " + encodeCredentials("scanner1", "1234"));
-            conn.setRequestProperty("Authorization", "Basic c2Nhbm5lcjE6MTIzNA==");  // Base64 encoded username:password
-
+          
+            conn.setRequestProperty("Authorization", "Basic " + encodeCredentials("PET4U", "SAPB1Admin"));
+            
             conn.setDoOutput(true);
 
             // Write JSON data to request body
