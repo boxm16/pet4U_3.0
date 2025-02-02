d8a87b1259050e09f0d27ebcc1636f359087176f
Michail Sitmalidis <boxm16@yahoo.com>
2025-02-02 15:43:34 +0200

    camelot position changind, phase when i change position on SAP DB via API

--- src/java/Camelot/CamelotItem/CamelotItemController.java
+++ src/java/Camelot/CamelotItem/CamelotItemController.java
@@ -1,6 +1,8 @@
 package Camelot.CamelotItem;
 
+import java.util.ArrayList;
 import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CamelotItemController {
+
+    @Autowired
+    CamelotItemDao camelotItemDao;
 
     @RequestMapping(value = "goForCamelotPositionChanign")
     public String goForCamelotPositionChanign(@RequestParam(name = "itemId") String itemId,
@@ -17,8 +22,9 @@ public class CamelotItemController {
             System.out.println("Somebody trying to breach encryption");
             return "index";
         }
-
+        ArrayList<String> camelotPickingPositions = camelotItemDao.getCamelotPickingPositions();
         model.addAttribute("itemId", itemId);
+        model.addAttribute("camelotPickingPositions", camelotPickingPositions);
         return "/camelot/camelotItem/camelotItemPositionChangingServant";
     }
 }
--- src/java/Camelot/CamelotItem/CamelotItemDao.java
+++ src/java/Camelot/CamelotItem/CamelotItemDao.java
@@ -5,9 +5,42 @@
  */
 package Camelot.CamelotItem;
 
+import Service.DatabaseConnectionFactory;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CamelotItemDao {
-    
+
+    @Autowired
+    private DatabaseConnectionFactory databaseConnectionFactory;
+
+    ArrayList<String> getCamelotPickingPositions() {
+        ArrayList<String> camelotPickingPositions = new ArrayList<>();
+        Connection connection = databaseConnectionFactory.getSapHanaConnection();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT Name FROM \"PETCAMELOT_UAT2\".\"BYT_V_PICKLOCATIONDETAILS\" ORDER BY \"PETCAMELOT_UAT2\".\"BYT_V_PICKLOCATIONDETAILS\".\"NAME\" ';");
+
+            while (resultSet.next()) {
+                String position = resultSet.getString("NAME");
+                camelotPickingPositions.add(position);
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(CamelotItemDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return camelotPickingPositions;
+    }
+
 }
