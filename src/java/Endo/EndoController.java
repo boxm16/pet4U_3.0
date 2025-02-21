08d8693e2f473a74372ea7cb17c432eede78c4b1
Michail Sitmalidis <boxm16@yahoo.com>
2025-02-22 01:46:32 +0200

    camelot delivery invoice creation SAP

--- src/java/Endo/EndoController.java
+++ src/java/Endo/EndoController.java
@@ -407,6 +407,10 @@ public class EndoController {
         ArrayList<Item> listValues = new ArrayList<Item>(pet4UItemsRowByRow.values());
         modelMap.addAttribute("pet4UItemsRowByRow", listValues);
 
+        ArrayList<AltercodeContainer> pet4UAllAltercodeContainers = endoDao.getAllAltercodeContainers();
+        System.out.println(pet4UAllAltercodeContainers.size());
+        modelMap.addAttribute("pet4UAllAltercodeContainers", pet4UAllAltercodeContainers);
+
         String saveButton = "<button class=\"btn-primary\" onclick=\"requestRouter('updateEndoDeliveryChecking.htm')\"><H1>UPDATE  ENDO Delivery Checking</H1></button>";
         modelMap.addAttribute("saveButton", saveButton);
         return "endo/endoDeliveryChecking";
