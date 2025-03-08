bfb46711e0b97d9a44199d9dac2eb543c6cc3f82
Michail Sitmalidis <boxm16@yahoo.com>
2025-03-08 02:18:14 +0200

    sap login

--- src/java/SAP/SapController.java
+++ src/java/SAP/SapController.java
@@ -77,82 +77,43 @@ public class SapController {
             String apiUrl = BASE_URL + "/Items";
 
             // JSON body for creating an item
-           String jsonBody = "{"
-                + "\"ItemCode\": \"F171909\", "
-                + "\"ItemName\": \"ΚΟΚΚΑΛΟ ΚΟΜΠΟΣ-6.35cm-10gr/10pcs (NEW)\", "
-                + "\"ItemsGroupCode\": 110, "
-                + "\"SalesUnit\": \"ΣΥΣΚΕΥΑΣΙΑ\", "
-                + "\"PurchaseUnit\": \"ΤΕΜΑΧΙΑ\", "
-                + "\"InventoryItem\": \"tYES\", "
-                + "\"SalesItem\": \"tYES\", "
-                + "\"PurchaseItem\": \"tYES\", "
-                + "\"VatLiable\": \"tYES\", "
-                + "\"SalesVATGroup\": \"Φ7000-24\", "
-                + "\"PurchaseVATGroup\": \"Φ2000-24\", "
-                + "\"Properties7\": \"tYES\", "  // Existing field
-                + "\"ItemUnitOfMeasurementCollection\": ["  // Add UoM details here
-                + "  {"
-                + "    \"UoMEntry\": 1, "  // UoMEntry for "Piece"
-                + "    \"BaseUoM\": \"tYES\", "  // Mark as base unit of measure
-                + "    \"AltQty\": 1.0"  // Alternate quantity (1 piece)
-                + "  },"
-                + "  {"
-                + "    \"UoMEntry\": 2, "  // UoMEntry for "Box"
-                + "    \"BaseUoM\": \"tNO\", "  // Not the base unit of measure
-                + "    \"AltQty\": 10.0"  // Alternate quantity (10 pieces per box)
-                + "  }"
-                + "],"
-                + "\"ItemBarCodeCollection\": ["  // Add barcodes here
-                + "  {"
-                + "    \"Barcode\": \"1234567890123\", "  // Barcode for 1 Piece
-                + "    \"UoMEntry\": 1"  // UoMEntry for "Piece"
-                + "  },"
-                + "  {"
-                + "    \"Barcode\": \"9876543210987\", "  // Barcode for 1 Box
-                + "    \"UoMEntry\": 2"  // UoMEntry for "Box"
-                + "  }"
-                + "],"
-                + "\"ItemPrices\": ["
-                + "  {"
-                + "    \"PriceList\": 1, "
-                + "    \"Price\": 1.3, "
-                + "    \"Currency\": \"EUR\""
-                + "  },"
-                + "  {"
-                + "    \"PriceList\": 2, "
-                + "    \"Price\": 2.32, "
-                + "    \"Currency\": \"EUR\""
-                + "  }"
-                + "],"
-                + "\"ItemWarehouseInfoCollection\": ["
-                + "  {"
-                + "    \"WarehouseCode\": \"AX-BAR\", "
-                + "    \"MinimalStock\": 0.0, "
-                + "    \"MaximalStock\": 0.0, "
-                + "    \"MinimalOrder\": 0.0"
-                + "  }"
-                + "],"
-                + "\"ItemPreferredVendors\": ["
-                + "  {"
-                + "    \"BPCode\": \"ΠΡΟ-000076\""
-                + "  }"
-                + "]"
-                + "}";
-
-            String jsonBody1 = "{"
-                    + "\"ItemCode\": \"F171909\", "
+            String jsonBody = "{"
+                    + "\"ItemCode\": \"1003-12222P\", "
                     + "\"ItemName\": \"ΚΟΚΚΑΛΟ ΚΟΜΠΟΣ-6.35cm-10gr/10pcs (NEW)\", "
                     + "\"ItemsGroupCode\": 110, "
+                    + "\"SalesUnit\": \"ΣΥΣΚΕΥΑΣΙΑ\", "
+                    + "\"PurchaseUnit\": \"ΤΕΜΑΧΙΑ\", "
                     + "\"InventoryItem\": \"tYES\", "
                     + "\"SalesItem\": \"tYES\", "
                     + "\"PurchaseItem\": \"tYES\", "
-                    + "\"Properties7\" : \"tYES\"," // This is for Αξεσουαρ, if i want food i choose Properties5 tYES
-
-                    + "\"ItemUnitOfMeasurementCollection\": [" // Minimal UoM details
+                    + "\"VatLiable\": \"tYES\", "
+                    + "\"SalesVATGroup\": \"Φ7000-24\", "
+                    + "\"PurchaseVATGroup\": \"Φ2000-24\", "
+                    + "\"Properties5\" : \"tYES\"," // This is for Αξεσουαρ, if i want food i choose Properties5 tYES
+                    + "\"BarCode\": \"1003-12222P\", " // Add BarCode here
+                    + "\"ItemPrices\": ["
                     + "  {"
-                    + "    \"UoMEntry\": 1, " // UoMEntry for "Piece"
-                    + "    \"BaseUoM\": \"tYES\", " // Mark as base unit of measure
-                    + "    \"AltQty\": 1.0" // Alternate quantity (1 piece)
+                    + "    \"PriceList\": 1, "
+                    + "    \"Price\": 1.3, "
+                    + "    \"Currency\": \"EUR\""
+                    + "  },"
+                    + "  {"
+                    + "    \"PriceList\": 2, "
+                    + "    \"Price\": 2.32, "
+                    + "    \"Currency\": \"EUR\""
+                    + "  }"
+                    + "],"
+                    + "\"ItemWarehouseInfoCollection\": ["
+                    + "  {"
+                    + "    \"WarehouseCode\": \"AX-BAR\", "
+                    + "    \"MinimalStock\": 0.0, "
+                    + "    \"MaximalStock\": 0.0, "
+                    + "    \"MinimalOrder\": 0.0"
+                    + "  }"
+                    + "],"
+                    + "\"ItemPreferredVendors\": ["
+                    + "  {"
+                    + "    \"BPCode\": \"ΠΡΟ-000076\""
                     + "  }"
                     + "]"
                     + "}";
