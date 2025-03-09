f152279d84b9bcc1f9de451aeae1dbaab64a2454
Michail Sitmalidis <boxm16@yahoo.com>
2025-03-09 19:37:01 +0200

    sap login

--- src/java/Camelot/CamelotItemSearch/CamelotItemSearchController.java
+++ src/java/Camelot/CamelotItemSearch/CamelotItemSearchController.java
@@ -36,6 +36,6 @@ public class CamelotItemSearchController {
 
         modelMap.addAttribute("items", items);
 
-        return "camelotItemSearch/singleItemSearchResultFromSapHana";
+        return "sap/allItems";
     }
 }
