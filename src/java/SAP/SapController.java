f3edc332a7e903ab77ee729af7a0a3ad82914922
Michail Sitmalidis <boxm16@yahoo.com>
2025-03-14 23:48:51 +0200

    sap working

--- src/java/SAP/SapController.java
+++ src/java/SAP/SapController.java
@@ -229,8 +229,8 @@ public class SapController {
             }
 
             JSONObject newBarcode = new JSONObject();
-            newBarcode.put("Barcode", "010000000120"); // New barcode
-            newBarcode.put("UoMEntry", 1); // Unit of Measure Entry
+            newBarcode.put("Barcode", "030000000120"); // New barcode
+            newBarcode.put("UoMEntry", 3); // Unit of Measure Entry
             newBarcode.put("FreeText", "TEMAXIA");//FreeText -dont know what it does
 
             barcodesArray.put(newBarcode);  // Append new barcode to the array
