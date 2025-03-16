e9de6d342937d1624b3e670ba73588a6734a0fd8
Michail Sitmalidis <boxm16@yahoo.com>
2025-03-16 18:18:55 +0200

    stil SAP

--- src/java/SAP/Camelot/CamelotItemSearch/CamelotItemSearchController.java
+++ src/java/SAP/Camelot/CamelotItemSearch/CamelotItemSearchController.java
@@ -6,10 +6,12 @@
 package SAP.Camelot.CamelotItemSearch;
 
 import BasicModel.Item;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class CamelotItemSearchController {
 
     @RequestMapping(value = "sapCamelotSearchDashboard")
