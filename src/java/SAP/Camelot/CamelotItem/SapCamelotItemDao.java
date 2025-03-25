c6f4e74d5952b7ab57ff39c641c99e1fb8f6013b
Michail Sitmalidis <boxm16@yahoo.com>
2025-03-25 20:09:09 +0200

    stil SAP

--- src/java/SAP/Camelot/CamelotItem/SapCamelotItemDao.java
+++ src/java/SAP/Camelot/CamelotItem/SapCamelotItemDao.java
@@ -13,6 +13,7 @@ import SAP.SapBasicModel.SapUnitOfMeasurement;
 import SAP.SapBasicModel.SapUnitOfMeasurementGroup;
 import Service.DatabaseConnectionFactory;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -98,39 +99,25 @@ public class SapCamelotItemDao {
     }
 
     SapItem getSapItemByItemCode(String itemCode) {
-        DatabaseConnectionFactory databaseConnectionFactory = new DatabaseConnectionFactory();
-        Connection connection = databaseConnectionFactory.getSapHanaConnection();
         SapItem item = null;
-        try {
-            Statement statement = connection.createStatement();
+        String query
+                = "SELECT "
+                + "  OITM.\"ItemCode\", OITM.\"ItemName\", OITM.\"CodeBars\", OITM.\"U_PickLocation\", "
+                + "  OBCD.\"BcdCode\", OBCD.\"BcdName\", "
+                + "  OUOM.\"UomEntry\", OUOM.\"UomCode\", OUOM.\"UomName\", "
+                + "  UGP1.\"BaseQty\", "
+                + "  OUGP.\"UgpEntry\", OUGP.\"UgpCode\", OUGP.\"UgpName\" "
+                + "FROM PETCAMELOT_UAT2.\"OITM\" OITM "
+                + "LEFT JOIN PETCAMELOT_UAT2.\"OUGP\" OUGP ON OITM.\"UgpEntry\" = OUGP.\"UgpEntry\" "
+                + "LEFT JOIN PETCAMELOT_UAT2.\"UGP1\" UGP1 ON OUGP.\"UgpEntry\" = UGP1.\"UgpEntry\" "
+                + "LEFT JOIN PETCAMELOT_UAT2.\"OUOM\" OUOM ON UGP1.\"UomEntry\" = OUOM.\"UomEntry\" "
+                + "LEFT JOIN PETCAMELOT_UAT2.\"OBCD\" OBCD ON OITM.\"ItemCode\" = OBCD.\"ItemCode\" "
+                + "  AND OBCD.\"UomEntry\" = OUOM.\"UomEntry\" "
+                + "WHERE OITM.\"ItemCode\" = ?";
+        try (Connection connection = new DatabaseConnectionFactory().getSapHanaConnection();
+                PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, itemCode);
             ResultSet resultSet = null;
-            String query = "SELECT "
-                    + " OITM.\"ItemCode\", "
-                    + " OITM.\"ItemName\", "
-                    + " OITM.\"CodeBars\", "
-                    + " OITM.\"U_PickLocation\", "
-                    + " OBCD.\"BcdCode\", "
-                    + " OBCD.\"BcdName\", "
-                    + " OUOM.\"UomEntry\", "
-                    + " OUOM.\"UomCode\", "
-                    + " OUOM.\"UomName\", "
-                    + " UGP1.\"BaseQty\", "
-                    + " OUGP.\"UgpEntry\", " // Added OUGP.UgpEntry
-                    + " OUGP.\"UgpCode\", " // Added OUGP.UgpCode
-                    + " OUGP.\"UgpName\" " // Added OUGP.UgpName
-                    + " FROM "
-                    + " PETCAMELOT_UAT2.\"OITM\" "
-                    + " LEFT JOIN " // Changed to LEFT JOIN
-                    + " PETCAMELOT_UAT2.OBCD ON PETCAMELOT_UAT2.OITM.\"ItemCode\" = PETCAMELOT_UAT2.OBCD.\"ItemCode\" "
-                    + " LEFT JOIN "
-                    + " PETCAMELOT_UAT2.OUOM ON OBCD.\"UomEntry\" = PETCAMELOT_UAT2.OUOM.\"UomEntry\" "
-                    + " LEFT JOIN "
-                    + " PETCAMELOT_UAT2.UGP1 ON OUOM.\"UomEntry\" = UGP1.\"UomEntry\" AND UGP1.\"UgpEntry\" = OITM.\"UgpEntry\" "
-                    + " LEFT JOIN " // Added LEFT JOIN for OUGP
-                    + " PETCAMELOT_UAT2.OUGP ON OITM.\"UgpEntry\" = OUGP.\"UgpEntry\" " // Join condition for OUGP
-                    + " WHERE "
-                    + " PETCAMELOT_UAT2.OITM.\"ItemCode\" = '" + itemCode + "';";
-
             resultSet = statement.executeQuery(query);
             int index = 0;
             while (resultSet.next()) {
@@ -177,7 +164,7 @@ public class SapCamelotItemDao {
                 if (resultSet.getString("BcdCode") == null) {
                     //do nothing
                 } else {
-                    if (resultSet.getString("CodeBars")!=null&&resultSet.getString("CodeBars").equals(resultSet.getString("BcdCode"))) {
+                    if (resultSet.getString("CodeBars") != null && resultSet.getString("CodeBars").equals(resultSet.getString("BcdCode"))) {
                         altercodeContainer.setMainBarcode(true);
                         //  item.setMainBarcode(resultSet.getString("ALTERNATECODE"));// HERE ALTERNATECODE AND MAIN_CODE IS THE SAME
                         item.setMainBarcode(resultSet.getString("BcdCode"));//
