b6a7db85b9b92b069103a4e5bdb8f9a89c7cbfcb
Michail Sitmalidis <boxm16@yahoo.com>
2025-04-04 23:54:23 +0300

    stil SAP

--- src/java/SAP/SapBasicModel/SapUnitOfMeasurementGroup.java
+++ src/java/SAP/SapBasicModel/SapUnitOfMeasurementGroup.java
@@ -4,7 +4,7 @@ import org.apache.commons.collections4.map.LinkedMap;
 
 public class SapUnitOfMeasurementGroup {
 
-    private int UgpEntry;
+    private Integer UgpEntry;
     private String UgpCode;
     private String UgpName;
     private LinkedMap<String, SapUnitOfMeasurement> unitOfMeasurements;
