c11ea803275cf3be65942d08dac8e00c5774ae99
Michail Sitmalidis <boxm16@yahoo.com>
2025-04-11 22:49:28 +0300

    sap

--- src/java/SAP/Camelot/CamelotDelivery/SapCamelotDeliveryDao.java
+++ src/java/SAP/Camelot/CamelotDelivery/SapCamelotDeliveryDao.java
@@ -6,8 +6,15 @@
 package SAP.Camelot.CamelotDelivery;
 
 import Delivery.DeliveryInvoice;
+import Service.DatabaseConnectionFactory;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -22,9 +29,10 @@ public class SapCamelotDeliveryDao {
     }
 
     LinkedHashMap<String, ArrayList<DeliveryInvoice>> getDuePurchaseOrders() {
-        LinkedHashMap<String, ArrayList<DeliveryInvoice>> getDuePurchaseOrders = new LinkedHashMap<>();
+        LinkedHashMap<String, ArrayList<DeliveryInvoice>> duePurchaseOrders = new LinkedHashMap<>();
+
         String query = "SELECT "
-                + dbSchema + ".\"DocNum\" AS PurchaseOrderNumber, "
+                + dbSchema + ".OPOR.\"DocNum\",  "
                 + dbSchema + ".OPOR.\"CardCode\", "
                 + dbSchema + ".OPOR.\"CardName\", "
                 + dbSchema + ".OPOR.\"DocDate\", "
@@ -39,8 +47,37 @@ public class SapCamelotDeliveryDao {
                 + dbSchema + " POR1 ON "
                 + dbSchema + " OPOR.\"DocEntry\" = " + dbSchema + ".POR1.\"DocEntry\"; ";
         System.out.println("Query: " + query);
-        
-        return getDuePurchaseOrders;
+
+        DatabaseConnectionFactory databaseConnectionFactory = new DatabaseConnectionFactory();
+        Connection connection = databaseConnectionFactory.getSapHanaConnection();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                String supplierName = resultSet.getString("CardName");
+                if (duePurchaseOrders.containsKey(supplierName)) {
+                    //do nothing
+                } else {
+                    duePurchaseOrders.put(supplierName, new ArrayList<>());
+                }
+                DeliveryInvoice purchaseOrderInvoice = new DeliveryInvoice();
+                purchaseOrderInvoice.setSupplier(supplierName);
+                purchaseOrderInvoice.setInvoiceId(resultSet.getString("DocNum"));
+
+                ArrayList<DeliveryInvoice> deliveryInvoices = duePurchaseOrders.get(supplierName);
+                deliveryInvoices.add(purchaseOrderInvoice);
+                duePurchaseOrders.put(supplierName, deliveryInvoices);
+
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(SapCamelotDeliveryDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return duePurchaseOrders;
     }
 
 }
