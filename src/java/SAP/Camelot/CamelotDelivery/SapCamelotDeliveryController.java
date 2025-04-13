eb5aaab21062a8b27fcfc2f19ba11321233e5331
Michail Sitmalidis <boxm16@yahoo.com>
2025-04-13 17:12:54 +0300

    sap

--- src/java/SAP/Camelot/CamelotDelivery/SapCamelotDeliveryController.java
+++ src/java/SAP/Camelot/CamelotDelivery/SapCamelotDeliveryController.java
@@ -281,7 +281,7 @@ public class SapCamelotDeliveryController {
         }
 
         SapCamelotDeliveryDao dao = new SapCamelotDeliveryDao();
-        String result = dao.saveSapTempoDeliveryChecking(invoiceNumber, supplierCode, invoiceNumber, deliveryItems);
+        String result = dao.saveSapTempoDeliveryChecking(invoiceId, supplierCode, invoiceNumber, deliveryItems);
         return "redirect:camelotDeliveryDashboardX.htm";
     }
 
@@ -343,7 +343,7 @@ public class SapCamelotDeliveryController {
         SapCamelotDeliveryDao dao = new SapCamelotDeliveryDao();
         String deleteResult = dao.deleteDeliveryChecking(invoiceId);
         String result = dao.saveSapTempoDeliveryChecking(invoiceId, supplierCode, invoiceNumber, deliveryItems);
-        return "redirect:deliveryDashboard_X.htm";
+        return "redirect:sapCamelotdeliveryDashboardX.htm";
     }
 
 }
